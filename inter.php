b51dd552c1042789742b300aa953059b1564a2b5
schoolgol <shahed357@gmail.com>
2016-09-21 16:01:10 +0300

    Add files via upload

---
+++ inter.php
@@ -0,0 +1,31 @@
+﻿<html dir="rtl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>المؤسسة العالمية الخاصة</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+<body style="padding:0px; margin:0px; background-color:#ffffff;font-family:Arial, sans-serif">
+
+
+	<center><img src="sass.jpg" width="800" height="500"></center>
+   <center> <a href="log.php" color="#FF0505"><h2><u><div style=" color:#FF0000; "><FONT face="Arial ,Tahoma" size="5">مرحباً بك في تطبيق السحابة</FONT></div></u></h2></a></center>
+
+</body>
+</html>
